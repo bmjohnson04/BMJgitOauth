2416056461616536c88b43022b4bb96020f28e5f
Brandon <bmjohnson04@gmail.com>
2016-05-13 15:29:28 -0400

    This is the initial commit of the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
 
+	 # config/routes.rb
+	root 'users#new'
+	get 'users/new'
+
+	get 'auth/:provider/callback' => 'sessions#create'
+	get 'auth/github', as: 'github_login'
+
+	get 'logout' => 'sessions#destroy'
+
 end
